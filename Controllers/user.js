1e9ce7295b349b39e4965870f1ccd393b6afed2a
Mr-Haseeb786 <m.haseebofficial786@gmail.com>
2024-07-24 18:45:47 +0500

    A lit tweaks in the API

--- Controllers/user.js
+++ Controllers/user.js
@@ -5,6 +5,7 @@ const {
   addFavourites,
 } = require("../utils/dbHandlers");
 const { createJwtToken, verifyToken } = require("../utils/jwtAuth");
+const { UserModel } = require("../Models/User");
 
 async function handleRegistNewUser(req, res) {
   const { username, password, email } = req.body;
@@ -20,6 +21,17 @@ async function handleRegistNewUser(req, res) {
   };
 
   try {
+    const emailExists = await UserModel.findOne({
+      email,
+    });
+
+    if (emailExists)
+      return res.status(400).json({ error: "Email Already exists" });
+  } catch (error) {
+    console.log("An error occured");
+  }
+
+  try {
     const newUser = await insertUser(user);
     return res.status(201).json({
       message: "New User Registered Successfully",
@@ -27,7 +39,7 @@ async function handleRegistNewUser(req, res) {
     });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ message: "Error Registering the user" });
+    return res.status(500).json({ error: "Error Registering the user" });
   }
 }
 
@@ -35,16 +47,16 @@ async function handleUserSignIn(req, res) {
   const { email, password } = req.body;
 
   if (!email || !password)
-    return res.status(400).json({ message: "No Email or Password Provided" });
+    return res.status(400).json({ error: "No Email or Password Provided" });
 
   try {
     const user = await getUserByEmail(email);
     if (!user)
-      return res.status(401).json({ message: "User Email Does not Exist" });
+      return res.status(401).json({ error: "User Email Does not Exist" });
 
     const passwordMatched = await bcrypt.compare(password, user.passwordHash);
     if (!passwordMatched)
-      return res.status(401).json({ message: "Incorrect Password" });
+      return res.status(401).json({ error: "Incorrect Password" });
 
     const userInfo = {
       userId: user._id,
@@ -58,7 +70,7 @@ async function handleUserSignIn(req, res) {
     return res.json({ message: "User LoggedIn ", userInfo });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Error searching for User" });
+    res.status(500).json({ error: "Error searching for User" });
   }
 }
 
@@ -84,7 +96,7 @@ async function handleSetUserFavourites(req, res) {
     !reviews ||
     !ratings
   ) {
-    return res.status(400).json({ message: "Product Details are missing" });
+    return res.status(400).json({ error: "Product Details are missing" });
   }
 
   let userDetails = null;
@@ -99,7 +111,7 @@ async function handleSetUserFavourites(req, res) {
   } catch (error) {
     console.log(error);
     return res.status(400).json({
-      message:
+      error:
         "Your Cookies have been cleared or altered. You need to login again to view Your Favourites",
     });
   }
@@ -111,7 +123,7 @@ async function handleSetUserFavourites(req, res) {
       .status(201)
       .json({ message: "Added to Favourites", favProduct: favProduct._id });
   } catch (error) {
-    res.json({ message: "There was an error inserting in db" });
+    res.json({ error: "There was an error inserting in db" });
   }
 }
 
